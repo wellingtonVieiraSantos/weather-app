bf2dcadf2d95cee240de60ff709330c75845ffdd
wellingtonVieiraSantos <wtvsantos@gmail.com>
2023-10-29 23:27:35 -0300

    add button search

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,7 +1,7 @@
 import {AiOutlineSearch, AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
 import { description } from '../../utils/description'
 
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { useContext } from 'react'
 import { WeatherContext } from '../../contexts/WeatherProvider/context'
 
@@ -12,19 +12,20 @@ const Search = () => {
     isError: false, 
     list: []
   })
-  const [locationName, setLocationName] = useState('')
   const [isOpen, setIsOpen] = useState(false)
 
   const { setWeatherInfo } = useContext(WeatherContext)
 
-  //chamada de api passando o valor digitado no input para obter uma lista de localizações
-  const handleSearch = async (e) =>{
+  const location = useRef(null)
 
-    setLocationName(e.target.value)
+  //chamada de api passando o valor digitado no input para obter uma lista de localizações
+  const handleSearch = async () =>{
+
+    if(!location.current.value) return
     setListLocations(prev=>({...prev, isLoading: true, isError: false}))
 
     try {
-      const url = `https://geocoding-api.open-meteo.com/v1/search?name=${e.target.value}&count=10&language=pt&format=json`
+      const url = `https://geocoding-api.open-meteo.com/v1/search?name=${location.current.value}&count=10&language=pt&format=json`
     
       const response = await fetch(url)
       const data = await response.json() 
@@ -42,7 +43,6 @@ const Search = () => {
   const handleFetch = async (location) => {
 
     setWeatherInfo(prev=>({...prev, isLoading: true, isError: false}))
-    setLocationName('')
     setIsOpen(false)
 
     try {
@@ -82,7 +82,6 @@ const Search = () => {
   const handleOpen = () =>{
     setIsOpen(prev => !prev)
     setListLocations(prev=>({...prev, isError: false, isLoading: false, list: []}))
-    setLocationName('')
   }
 
   return (
@@ -95,11 +94,15 @@ const Search = () => {
         </div>
         {isOpen &&
           <div className="relative my-2 text-center">
-            <AiOutlineSearch className="absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-28 text-slate-400 text-2xl"/>
-            <input type="text" className="p-2 pl-12 text-slate-800 font-montserrat font-semibold outline-none border rounded
-             bg-slate-50 shadow" value={locationName} onChange={(e)=>handleSearch(e)} placeholder="Insira o local"/>
+            <div className='h-12 flex justify-center gap-2'>
+              <input type="text" className="p-2 px-4 text-slate-800 font-montserrat font-semibold outline-none border rounded
+               bg-slate-50 shadow" ref={location} placeholder="Insira o local"/>
+              <button className='bg-blue-950 p-2 aspect-square flex justify-center items-center rounded border'
+                onClick={handleSearch}
+              ><AiOutlineSearch className='text-4xl'/></button>
+            </div>
             <ul className={`w-2/3 sm:w-auto m-3 flex flex-col absolute z-10 top-12 left-1/2 -translate-x-1/2 before:absolute before:w-0 before:h-0 before:border-l-8 before:border-l-transparent 
-            before:border-r-8 before:border-r-transparent before:border-b-8 before:border-b-blue-900 before:-top-2 before:right-1/2 ${!locationName && 'before:opacity-0'}`}>
+            before:border-r-8 before:border-r-transparent before:border-b-8 before:border-b-blue-900 before:-top-2 before:right-1/2 ${!location.current && 'before:opacity-0'}`}>
             {listLocations.isError && <li className="bg-blue-900 text-slate-200 p-2 font-montserrat">Ocorreu um erro...</li> }
             {listLocations.isLoading && <li className="bg-blue-900 text-slate-200 p-2 font-montserrat">Aguarde, carregando locais...</li> }
             {listLocations.list.length && !listLocations.isLoading ? listLocations.list.map((location)=>(
@@ -107,7 +110,7 @@ const Search = () => {
                 <li className="bg-blue-900 text-slate-200 p-2 font-montserrat text-lg" >{location.name}, <span className="text-sm">{location.admin1} - {location.country}</span></li>
               </button>
             )) :
-              locationName && !listLocations.isLoading ?
+              location.current && !listLocations.isLoading ?
               <li className="bg-blue-900 text-slate-200 p-2 font-montserrat" >Nenhum local encontrado...</li> 
               : ''
             }
